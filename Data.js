bdec8129a48f9f9c2cb082af4b9b546d1c8912e2
krushabh-dev <rushabhrbs@gmail.com>
2022-12-20 22:24:25 +0530

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,14 @@
+const Data = [{
+  symbole: "$",
+  text: "USD- US Dollar",
+  exchangeRate: "1USD=1USD",
+  img: "https://img.freepik.com/free-vector/illustration-usa-flag_53876-18165.jpg?size=626&ext=jpg"
+},{
+  symbole: "₹",
+  text: "INR- Indian Rupees",
+  exchangeRate: "1USD=82.66INR",
+  img: "https://img.freepik.com/free-vector/illustration-india-flag_53876-27130.jpg?auto=format&h=200"
+},
+]
+
+export default Data
